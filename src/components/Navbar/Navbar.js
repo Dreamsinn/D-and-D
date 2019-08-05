1791a1cd7a830d3397f93c5900f6bc58c9f5e76b
Jordi Santamaria <santjordiporto@hotmail.com>
2019-08-06 00:10:57 +0200

    included image and prettier

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,18 +2,20 @@ import React from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 import DropDown from "../sharedComponents/dropdown";
+import logo from "../../logo.svg";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="leftPart">
         <Link to="/" className="navbarLeftItem">
-          Home
+          <img width={"30px"} height={"30px"} src={logo} alt="logo" />
         </Link>
         <div className="navbarLeftItem">Descargar</div>
         <DropDown className="navbarLeftItem">
           {open => {
-            return open  <div>How to </div> : <div>How NOT to </div>;
+            /*&& ==> if    ? : if else*/
+            return open ? <div>How to </div> : <div>How NOT to </div>;
           }}
         </DropDown>
       </div>
